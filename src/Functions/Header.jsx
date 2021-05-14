935f812aa748b48bb2595e969e691bc8295fbf99
Laith Al-Saadoon <alsaadoonlaith@gmail.com>
2021-05-14 13:41:49 -0500

    basic functionality is here

--- src/Functions/Header.jsx
+++ src/Functions/Header.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { Paper } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 function Header() {
   return (
     <Grid item xs={10}>
-      <Typography variant="h2" style={{ textAlign: "center" }}>
-        AWS Panorama | Smart Shelf Demo
-      </Typography>
+      <Paper style={{ margin: 10 }}>
+        <Typography variant="h2" style={{ textAlign: "center" }}>
+          AWS Panorama | Smart Shelf Demo
+        </Typography>
+      </Paper>
     </Grid>
   );
 }
