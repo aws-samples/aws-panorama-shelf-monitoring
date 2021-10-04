15fd4e24300fd36ee385c5842b99777122e7b814
Laith Al-Saadoon <alsaadoonlaith@gmail.com>
2021-10-04 11:51:09 -0500

    delay sns

--- amplify/backend/function/smartshelf72d32494/src/index.js
+++ amplify/backend/function/smartshelf72d32494/src/index.js
@@ -1,19 +1,45 @@
+/* eslint-disable new-cap */
 /* eslint-disable no-console */
 const { unmarshall } = require("@aws-sdk/util-dynamodb");
 const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
-const { Done } = require("@material-ui/icons");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 
 const REGION = process.env.AWS_REGION;
 const sns = new SNSClient({ region: REGION });
+const client = new DynamoDBClient({region: REGION});
+const ddbDocClient = DynamoDBDocument.from(client);
+const delay = process.env.DELAY || 10;
 
 
-const sendAlert = async () => {
+const sendAlert = async (bottleCount, threshold) => {
   try {
     const data = await sns.send(new PublishCommand({
       TopicArn: process.env.SNS_TOPIC,
-      Message: "The bottle shelf is low. Please restock ASAP."
+      Message: `The item count is currently ${bottleCount}, and the alert threshold is set to ${threshold}. Please restock as soon as possible.`
     }))
     console.log("Message send successfully!", data);
+  } catch (err) {
+    console.error(err, err.stack);
+  }
+}
+
+
+const saveAlertTime = async (tableName, now) => {
+  const params = {
+    TableName: tableName,
+    Key: {
+      "ProductType": "BOTTLE",
+    },
+    UpdateExpression: "set lastNotificationSent = :a",
+    ExpressionAttributeValues: {
+      ":a": now,
+    },
+  };
+
+  try {
+    const data = await ddbDocClient.update(params);
+    console.log("Time saved successfully!", data);
   } catch (err) {
     console.error(err, err.stack);
   }
@@ -25,16 +51,18 @@ exports.handler = event => {
   event.Records.forEach(record => {
     const newRecord = unmarshall(record.dynamodb.NewImage);
     const oldRecord = unmarshall(record.dynamodb.OldImage);
+    const tableName = record.eventSourceARN.split("/")[1];
+    
+    const bottleCount = newRecord.count;
+    const threshold = newRecord.Threshold;
 
     if (oldRecord.Threshold !== newRecord.Threshold) {
       console.log("Nothing new, just updating the threshold...");
       return;
     }
 
-    const bottleCount = newRecord.count;
-    const threshold = newRecord.Threshold;
-
     if (bottleCount === 9000) {
+      // magic count value when updating a threshold.
       return;
     }
 
@@ -42,10 +70,25 @@ exports.handler = event => {
     /* TODO: don't send alert if
     notification was sent <10 mins ago and if threshold hasn't changed */
     if (bottleCount <= threshold) {
-      sendAlert();
-    } else {
-      return;
-    }
+      if (newRecord.lastNotificationSent) {
+        const lastNotificationTime = new Date(newRecord.lastNotificationSent);
+        const now = new Date.now();
+        const diff = now - lastNotificationTime;
+        const diffMinutes = diff / 1000 / 60;
+        if (diffMinutes < delay) {
+          console.log("Not sending alert because it was sent less than 10 minutes ago");
+        } else {
+          console.log("Sending alert because it was sent more than 10 minutes ago");
+          sendAlert(bottleCount, threshold);
+          saveAlertTime(tableName, now);
+        }
+      } else {
+        const now = Date.now();
+        console.log("Sending alert because it was never sent");
+        sendAlert(bottleCount, threshold);
+        saveAlertTime(tableName, now);
+      }
+    };
 
     console.log(record.eventID);
     console.log(record.eventName);
